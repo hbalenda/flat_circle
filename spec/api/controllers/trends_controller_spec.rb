6f611c6f2f0305bc2020002ea890bbca014d182a
hcbviolet <hcbviolet@gmail.com>
2016-05-02 15:52:21 -0700

    Validate uniqueness of trend name and test

--- spec/api/controllers/trends_controller_spec.rb
+++ spec/api/controllers/trends_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Api::TrendsController, type: :controller do
-  let(:user) { create(:user) }
-  let(:trend) { create(:trend, user_id: user.id) }
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    @trend = FactoryGirl.create(:trend, user_id: @user.id)
+  end
 
   context 'Unauthenticated user' do
     it 'GET index returns http unauthenticated' do
@@ -11,19 +13,19 @@ RSpec.describe Api::TrendsController, type: :controller do
     end
 
     it 'POST create returns http unauthenticated' do
-      post :create, params: { trend: { name: trend.name, user_id: trend.user_id } }
+      post :create, params: { trend: { name: @trend.name, user_id: @trend.user_id } }
       expect(response).to have_http_status(401)
     end
 
     it 'DELETE destroy returns http unauthenticated' do
-      delete :destroy, id: trend.id
+      delete :destroy, params: { id: @trend.id }
       expect(response).to have_http_status(401)
     end
   end
 
   context 'Authenticated user' do
     before do
-      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(user.auth_token)
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(@user.auth_token)
     end
 
     it 'GET index returns http authenticated' do
@@ -37,7 +39,7 @@ RSpec.describe Api::TrendsController, type: :controller do
     end
 
     describe 'POST create' do
-      before { post :create, params: { trend: { name: trend.name, user_id: trend.user_id } } }
+      before { post :create, params: { trend: { name: "New Trend", user_id: @trend.user_id } } }
 
       it 'POST create returns http authenticated' do
         expect(response).to have_http_status(:success)
@@ -49,12 +51,12 @@ RSpec.describe Api::TrendsController, type: :controller do
 
       it 'creates a trend with the correct attributes' do
         hashed_json = JSON.parse(response.body)
-        expect(hashed_json['name']).to eq(trend.name)
+        expect(hashed_json['name']).to eq("New Trend")
       end
     end
 
     describe 'DELETE destroy' do
-      before { delete :destroy, id: trend.id }
+      before { delete :destroy, params: { id: @trend.id } }
 
       it 'DELETE destroy returns http authenticated' do
         expect(response).to have_http_status(:success)
--- spec/models/trend_spec.rb
+++ spec/models/trend_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Trend, type: :model do
   let(:user) { create(:user) }
   let(:trend) { create(:trend) }
   it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
   it { is_expected.to belong_to(:user) }
 end
