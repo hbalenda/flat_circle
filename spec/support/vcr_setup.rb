6e3dfb344531a46dc8995cb85d2064d856386ada
hcbviolet <hcbviolet@gmail.com>
2016-05-08 21:38:38 -0700

    Remove VCR and Webmock

--- spec/support/vcr_setup.rb
+++
@@ -1,9 +0,0 @@
-require 'vcr'
-require 'webmock'
-
-VCR.configure do |c|
-  c.hook_into :webmock
-  c.cassette_library_dir = 'spec/vcr'
-  c.configure_rspec_metadata!
-  c.default_cassette_options = { record: :new_episodes }
-end
