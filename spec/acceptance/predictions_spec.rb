9369f01f7837442f8fba23d55a30826ab243b74d
hcbviolet <hcbviolet@gmail.com>
2016-05-11 17:48:10 -0700

    Update prediction specs

--- spec/acceptance/predictions_spec.rb
+++ spec/acceptance/predictions_spec.rb
@@ -10,13 +10,9 @@ resource 'Predictions' do
     200.times { FactoryGirl.create(:occurrence, trend_id: Trend.all.sample.id) }
   end
 
-  get 'api/predictions/:prediction_id' do
-    parameter :prediction_id, 'Prediction ID'
-
-    let(:prediction_id) { @prediction.id }
-
+  get 'api/predictions' do
     example 'Reveal a Prediction' do
-      explanation 'Predictions are created with a "year" attribute (e.g. 2082 or 1970). GET api/predictions/:id combs through the database and returns trends that were or will be "in" that year.'
+      explanation 'Predictions have a "year" attribute (e.g. 2082 or 1970). This request combs through the database and returns trends that were or will be "in" that year.'
       do_request
       expect(status).to eq(200)
     end
